12f42909b715314677da1317843248fbbd1ef11e
Temesgen Tsegaye <temesgentsegaye9891@gmail.com>
2024-07-10 08:57:54 +0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     typescript:{
       ignoreBuildErrors:true
-    }
+    },
     images: {
       domains: ['dummyimage.com'],
       remotePatterns: [
